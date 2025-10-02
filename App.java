acdef52a83eee3f0b8ca60aaf9889ed197a0f93b
Tulsi Shah <tushah23@gmail.com>
2025-10-02 10:38:56 -0500

    App.java

--- App.java
+++ App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to Fitness Tracekr!");
+       Scanner sc = new Scanner(System.in);
+
+       System.out.println("Enter your name: "); 
+       String name = sc.nextLine(); 
+
+       System.out.println("Enter your age: "); 
+       int age = sc.nextInt(); 
+
+       System.out.println("Enter your weight (lbs): "); 
+       double weight = sc.nextDouble(); 
+
+       System.out.println("Enter your height (inches): "); 
+       double height = sc.nextDouble(); 
+
+       System.out.println("Enter your goal: ");
+       String goal = sc.nextLine(); 
+
+       User user = new User(name, age, weight, height, goal); 
+
+       System.out.println("\nProfile created successfully!");
+       System.out.println(user); 
+
+       sc.close();
+
     }
 }
