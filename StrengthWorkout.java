eaa176befa9599d870e0e5f26f0036caa8863400
Tulsi Shah <tushah23@gmail.com>
2025-10-06 19:22:31 -0500

    StrengthWorkout.java

---
+++ StrengthWorkout.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class StrengthWorkout extends Workout {
+    private final double sets;
+    private final double reps; // per set (hopfully its the same for each set)
+    private final double weight; // in lbs
+    private final String exerciseName; 
+    private final List<Double> currentSetRepWeight = new ArrayList<>();
+    private final Map<String, List<Double>> liftLog = new HashMap<>();
+
+    public StrengthWorkout(String initType, String initEquipment, String initIntensity, double initSets, double initReps, double initWeight, String initExerciseName) {
+        super(initType, initEquipment, initIntensity);
+        this.sets = initSets;
+        this.reps = initReps;
+        this.weight = initWeight;
+        this.exerciseName = initExerciseName;
+        currentSetRepWeight.add(sets); 
+        currentSetRepWeight.add(reps);
+        currentSetRepWeight.add(weight);
+        liftLog.put(exerciseName, currentSetRepWeight); 
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Strength Workout Details: ");
+        for (String key : liftLog.keySet()) {
+            System.out.println("Exercise: " + key); 
+            System.out.println("Sets " + liftLog.get(key).get(0));
+            System.out.println("Reps per set" + liftLog.get(key).get(1));
+            System.out.println("Weight: " + liftLog.get(key).get(2));
+            System.out.println("Equipment: " + super.equipment); 
+        }
+        System.out.println("Intensity: " + super.intensity);
+
+    }
+
+    
+}
